e7858f8593cb4d56122e75db2e7e96864f7b09d0
shaneee123 <ysshane98@skuniv.ac.kr>
2024-11-14 00:42:01 -0800

    home screen, my profile screen, menu bar

--- src/user_pages/Home.tsx
+++ src/user_pages/Home.tsx
@@ -2,35 +2,36 @@ import React from 'react';
 
 const Home: React.FC = () => {
     return (
-        <div className="w-full p-4 text-black min-h-screen overflow-y-auto">
-            <div className="w-full p-6 space-y-6">
+        <div className="w-full p-8 text-black min-h-screen overflow-y-auto">
+            <div className="w-full space-y-6">
                 {/* Genre */}
-                <div className="flex flex-col items-center space-y-6">
+               
                     
                     {/* 첫 번째 이미지 */}
-                    <div className="w-full">
-                        <div className="text-left font-semibold opacity-90">
+                    <div className="w-full max-w-lg max-h-md mx-auto">
+                        <div className="text-left font-semibold opacity-90 mb-2">
                             #추리 #소설
                         </div>
-                        <div className="border rounded-lg overflow-hidden shadow-lg">
-                            <img 
-                                src="/images/image 63.png" 
-                                alt="Romance Genre Cover" 
-                                className="w-full"
-                            />
-                        </div>
+                    <div className="rounded-2xl overflow-hidden shadow-lg">
+                        <img 
+                            src="/images/choori.jpeg" 
+                            alt="Romance Genre Cover" 
+                            className="w-full h-[600px] rounded-2xl object-cover"
+                         />
                     </div>
+                    </div>
+
                     
                     {/* 두 번째 이미지 */}
                     <div className="w-full">
                         <div className="text-left font-semibold opacity-90">
                             # 로맨스 # 판타지
                         </div>
-                        <div className="border rounded-lg overflow-hidden shadow-lg">
+                        <div className=" rounded-lg overflow-hidden shadow-lg">
                             <img 
-                                src="/images/image 63.png" 
+                                src="/images/love.png" 
                                 alt="Romance Genre Cover" 
-                                className="w-full"
+                                className="w-full h-[600px] "
                             />
                         </div>
                     </div>
@@ -40,11 +41,11 @@ const Home: React.FC = () => {
                         <div className="text-left font-semibold opacity-90">
                             # 추리 # 미스테리
                         </div>
-                        <div className="border rounded-lg overflow-hidden shadow-lg">
+                        <div className=" rounded-lg overflow-hidden shadow-lg">
                             <img 
-                                src="/images/image 63.png" 
+                                src="/images/raise.png" 
                                 alt="Mystery Genre Cover" 
-                                className="w-full object-cover h-64 md:h-80 lg:h-96"
+                                className="w-full h-[600px] "
                             />
                         </div>
                     </div>
@@ -54,15 +55,15 @@ const Home: React.FC = () => {
                         <div className="text-left font-semibold opacity-90">
                             # 로맨스 # 판타지
                         </div>
-                        <div className="border rounded-lg overflow-hidden shadow-lg">
+                        <div className=" rounded-lg overflow-hidden shadow-lg">
                             <img 
-                                src="/images/image 63.png" 
+                                src="/images/detective.jpg" 
                                 alt="Romance Genre Cover" 
-                                className="w-full object-cover h-64 md:h-80 lg:h-96"
+                                className="w-full h-[600px]"
                             />
                         </div>
                     </div>
-                </div>
+                
             </div>
         </div>
     );
