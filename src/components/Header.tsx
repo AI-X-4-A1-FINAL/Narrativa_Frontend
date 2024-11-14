0d5e6f674f3c07cf1791267b9352d1a20a5d41e8
shaneee123 <ysshane98@skuniv.ac.kr>
2024-11-13 18:54:10 -0800

    홈페이지 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,24 @@ const Header: React.FC = () => {
     if (location.pathname === "/") {
         return null;
     }
-    return (
-        <header className="flex justify-around items-center w-full max-w-lg mx-auto p-4 text-black fixed top-0 h-16 bg-white" font-custom>
-            <Link to="/home">
-                <span className="text-4xl font-bold mr-auto">Narrativa</span>
-            </Link>
-            <Link to="/profile">
-                <img src="/images/myinfobutton.png" alt="Bookmarks" className="w-8 h-8" />
-            </Link>
 
+    return (
+        <header className="flex flex-col items-center w-full max-w-lg mx-auto p-4 text-black fixed top-0 bg-white z-10">
+            {/* 상단 이미지 컨테이너 */}
+            <div className="flex justify-between items-center w-full">
+                <Link to="/home" className='self-end'>
+                    <img src="/images/Group 18317.png" alt="Bookmarks" className="h-16 mt-6 ml-4"/>
+                </Link>
+                <Link to="/profile" className='self-end'>
+                    <img src="/images/infoButton.png" alt="Profile" className="w-10 h-10 mr-4" />
+                </Link>
+            </div>
+
+            {/* 박스 추가 */}
+            <div className="mt-4 bg-gray-100">
+                {/* 박스 내부의 내용을 여기에 추가하세요 */}
+                <p className="text-center text-gray-700"> 📣 공지사항 : 2024년 12월 20일 앱 배포 📣 </p>
+            </div>
         </header>
     );
 };
