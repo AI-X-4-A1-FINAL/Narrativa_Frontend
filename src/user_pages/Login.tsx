a76e20a3b57f1254b27f318343e7c6cdd53930cc
Yesssung <cjsrnr0504@naver.com>
2024-11-14 10:49:18 +0900

    로그인 버튼 호버추가

--- src/user_pages/Login.tsx
+++ src/user_pages/Login.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Login: React.FC = () => {
   return (
         <div className="w-full max-w-xs space-y-4">
-            <button className="w-60 py-3 text-yellow-800 bg-custom-yellow rounded-md flex justify-center items-center">
+            <button className="w-60 py-3 text-yellow-800 bg-custom-yellow rounded-md flex justify-center items-center hover:bg-yellow-400">
               <img src="/images/kakao.png" alt="Kakao Icon" className="w-5 h-5 mr-2" />
               <span>Login with Kakao</span>
             </button>
-            <button className="w-60 py-3 text-black bg-white border-black border rounded-md flex justify-center items-center">
+            <button className="w-60 py-3 text-black bg-white border-black border rounded-md flex justify-center items-center hover:bg-gray-200">
               <img src="/images/google_icon.png" alt="Google Icon" className="w-5 h-5 mr-2" />
               <span>Login with Google</span>
             </button>
-            <button className="w-full py-3 text-white bg-black rounded-md flex justify-center items-center">
+            <button className="w-full py-3 text-white bg-black rounded-md flex justify-center items-center hover:bg-gray-700">
               <img src="/images/github-mark-white.png" alt="GitHub Icon" className="w-5 h-5 mr-2" />
               <span>Login with GitHub</span>
             </button>
