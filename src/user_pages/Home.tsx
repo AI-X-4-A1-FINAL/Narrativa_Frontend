1fa997531f03cd9af2232924403ac97d83c75fdd
shaneee123 <ysshane98@skuniv.ac.kr>
2024-11-14 21:49:49 -0800

    홈 북마크 프로필 수정

--- src/user_pages/Home.tsx
+++ src/user_pages/Home.tsx
@@ -16,21 +16,6 @@ const Home: React.FC = () => {
     };
     
     return (
-        <div className="flex justify-around items-center w-full max-w-lg mx-auto p-4 text-black bottom-0 min-h-screen overflow-y-auto">
-            <div className="container mx-auto p-6">
-                <div className="border-b border-gray-300 pb-4 mb-4">
-                    <h2 className="text-lg font-semibold">공지사항 :</h2>
-                </div>
-                <div className="flex flex-col items-center space-y-6">
-                    <div className="w-full max-w-md">
-                        <div className="text-left font-semibold opacity-90">
-                                #추리 #미스테리
-                        </div>
-                        <div
-                            className="border rounded-lg overflow-hidden shadow-lg cursor-pointer"
-                            onClick={() => handleClick('추리', ['추리', '미스테리'], '/images/choori.png')}
-                        >
-
         <div className="w-full p-8 text-black min-h-screen overflow-y-auto">
             <div className="w-full space-y-6">
                 {/* Genre */}
@@ -82,11 +67,7 @@ const Home: React.FC = () => {
                             />
                         </div>
                     </div>
-
                     
-                    <div className="w-full max-w-md">
-                        <div className="text-left font-semibold opacity-90">
-                                #연애 #로맨스 #판타지
                     {/* 네 번째 이미지 */}
                     <div className="w-full">
                         <div className="text-left font-semibold opacity-90">
@@ -100,23 +81,11 @@ const Home: React.FC = () => {
                                 onClick={() => handleClick('연애', ['연애', '시뮬레이션', '로맨스'], '/images/love.png')}
                             />
                         </div>
-                        <div
-                                className="border rounded-lg overflow-hidden shadow-lg cursor-pointer"
-                                onClick={() => handleClick('연애', ['연애', '시뮬레이션', '로맨스'], '/images/love.png')}
-                            >
-                                <img 
-                                    src="/images/love.png" 
-                                    alt="Romance" 
-                                    className="w-full h-64 object-cover"
-                                />
-                            </div>
-                        
                     </div>
                 
             </div>
         </div>
     );
-
 };
 
 export default Home;
