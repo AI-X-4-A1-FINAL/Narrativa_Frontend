b497d5a81252824141187f006f5af66993935cb2
Yesssung <cjsrnr0504@naver.com>
2024-11-16 00:33:32 +0900

    장르별 이미지 수정, 태그 수정, 프로필 사진 수정

--- src/user_pages/Home.tsx
+++ src/user_pages/Home.tsx
@@ -18,23 +18,20 @@ const Home: React.FC = () => {
   return (
     <div className="w-full p-8 text-black min-h-screen overflow-y-auto">
       <div className="w-full space-y-6">
-        {/* Genre */}
-
-        {/* 첫 번째 이미지 */}
         <div className="w-full max-w-lg max-h-md mx-auto">
           <div className="text-left font-semibold opacity-90 mb-2">
             #생존 #아포칼립스
           </div>
           <div className="rounded-2xl overflow-hidden shadow-lg">
             <img
-              src="/images/choori.jpeg"
-              alt="Romance Genre Cover"
+              src="/images/survival.jpeg"
+              alt="Survival Genre Cover"
               className="w-full h-[600px] rounded-2xl object-cover"
               onClick={() =>
                 handleClick(
                   "생존",
                   ["아포칼립스", "생존", "재난"],
-                  "/images/choori.jpeg"
+                  "/images/survival.jpeg"
                 )
               }
             />
@@ -48,14 +45,14 @@ const Home: React.FC = () => {
           </div>
           <div className=" rounded-lg overflow-hidden shadow-lg">
             <img
-              src="/images/love.png"
+              src="/images/romance.png"
               alt="Romance Genre Cover"
               className="w-full h-[600px] "
               onClick={() =>
                 handleClick(
                   "연애",
                   ["연애", "시뮬레이션", "로맨스", "로맨스 판타지"],
-                  "/images/love.png"
+                  "/images/romance.png"
                 )
               }
             />
@@ -69,14 +66,14 @@ const Home: React.FC = () => {
           </div>
           <div className=" rounded-lg overflow-hidden shadow-lg">
             <img
-              src="/images/raise.png"
-              alt="Mystery Genre Cover"
+              src="/images/growth.png"
+              alt="Growth Genre Cover"
               className="w-full h-[600px] "
               onClick={() =>
                 handleClick(
                   "성장물",
                   ["육성", "시뮬레이션", "키우기", "성장물"],
-                  "/images/love.png"
+                  "/images/growth.png"
                 )
               }
             />
@@ -90,15 +87,11 @@ const Home: React.FC = () => {
           </div>
           <div className=" rounded-lg overflow-hidden shadow-lg">
             <img
-              src="/images/detective.jpg"
-              alt="Romance Genre Cover"
+              src="/images/mystery.png"
+              alt="Mystery Genre Cover"
               className="w-full h-[600px]"
               onClick={() =>
-                handleClick(
-                  "추리",
-                  ["추리", "미스테리", "모험"],
-                  "/images/love.png"
-                )
+                handleClick("추리", ["추리", "미스테리"], "/images/mystery.png")
               }
             />
           </div>
