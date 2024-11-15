6ceecc604252648cb547f2785f2b584ee9c52cca
Yesssung <cjsrnr0504@naver.com>
2024-11-15 15:59:31 +0900

    메인페이지 클릭시 로그인페이지로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Main from './user_pages/Main';
-import Login from './user_pages/Login';
-import Home from './user_pages/Home';
-import Profile from './user_pages/Profile';
-import Bookmarks from './user_pages/Bookmarks';
-import Settings from './user_pages/Settings';
-import DeleteAccount from './user_pages/DeleteAccount';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import GameIntro from './components/GameIntro';
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Main from "./user_pages/Main"
+import Login from "./user_pages/Login"
+import Home from "./user_pages/Home"
+import Profile from "./user_pages/Profile"
+import Bookmarks from "./user_pages/Bookmarks"
+import Settings from "./user_pages/Settings"
+import DeleteAccount from "./user_pages/DeleteAccount"
+import Header from "./components/Header"
+import GameIntro from "./components/GameIntro"
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen items-center justify-between bg-black">
         <Header />
-        <main className="flex-grow pt-32 px-4 w-full max-w-xl mx-auto bg-white">
+        <main className="flex-grow pt-40 px-4 w-full max-w-xl mx-auto bg-white">
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
@@ -28,10 +27,9 @@ const App: React.FC = () => {
             <Route path="/game-intro" element={<GameIntro />} />
           </Routes>
         </main>
-        
-      </div>  
+      </div>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
