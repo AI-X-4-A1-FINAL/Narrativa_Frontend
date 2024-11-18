95d6f2c789412453d6e6d50f0bbde7dfaa79ee5b
sjs <wltkqdl@gmail.com>
2024-11-18 21:56:46 +0900

    fix: redirect url 코드 수정

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,18 +1,10 @@
+
+
+const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID
+const auth_code_path = process.env.REACT_APP_GITHUB_AUTH_CODE_PATH
+
 export const getGithubLoginLink = (): string => {
-    // 환경 변수 값 가져오기
-    const { REACT_APP_GITHUB_CLIENT_ID, REACT_APP_GITHUB_AUTH_CODE_PATH } = process.env;
-
-    // 환경 변수들이 제대로 설정되어 있는지 확인
-    if (!REACT_APP_GITHUB_CLIENT_ID || !REACT_APP_GITHUB_AUTH_CODE_PATH) {
-        throw new Error("GitHub 환경 변수 설정이 누락되었습니다.");
-    }
-
-    // URL 인코딩을 적용하여 안전하게 값을 처리합니다.
-    const clientId = encodeURIComponent(REACT_APP_GITHUB_CLIENT_ID);
-    const authCodePath = REACT_APP_GITHUB_AUTH_CODE_PATH;
-
-    // GitHub 로그인 링크 생성
-    const githubURL = `${authCodePath}?client_id=${clientId}`;
+    const githubURL = `${auth_code_path}?client_id=${client_id}`;
 
     return githubURL;
 }
--- src/api/kakaoApi.ts
+++ src/api/kakaoApi.ts
@@ -1,18 +1,13 @@
+
+
+const rest_api_key = process.env.REACT_APP_KAKAO_CLIENT_ID
+const auth_code_path = process.env.REACT_APP_KAKAO_AUTH_CODE_PATH
+const react_app_uri = process.env.REACT_APP_SPRING_URI || ''
+const redirect_uri_part = process.env.REACT_APP_KAKAO_REDIRECT_URI_PART || ''
+const redirect_uri = react_app_uri + redirect_uri_part
+
 export const getKakaoLoginLink = (): string => {
-    // 필요한 환경 변수 값을 가져옵니다.
-    const { REACT_APP_KAKAO_CLIENT_ID, REACT_APP_KAKAO_AUTH_CODE_PATH, REACT_APP_SPRING_URI, REACT_APP_KAKAO_REDIRECT_URI_PART } = process.env;
-
-    // 환경 변수들이 제대로 설정되어 있는지 확인
-    if (!REACT_APP_KAKAO_CLIENT_ID || !REACT_APP_KAKAO_AUTH_CODE_PATH || !REACT_APP_SPRING_URI || !REACT_APP_KAKAO_REDIRECT_URI_PART) {
-        throw new Error("환경 변수 설정이 누락되었습니다.");
-    }
-
-    // URL 인코딩을 적용하여 안전하게 값을 처리합니다.
-    const clientId = encodeURIComponent(REACT_APP_KAKAO_CLIENT_ID);
-    const redirectUri = encodeURIComponent(REACT_APP_SPRING_URI + REACT_APP_KAKAO_REDIRECT_URI_PART);
-
-    // 카카오 로그인 링크를 생성
-    const kakaoURL = `${REACT_APP_KAKAO_AUTH_CODE_PATH}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
+    const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
     return kakaoURL;
 }
