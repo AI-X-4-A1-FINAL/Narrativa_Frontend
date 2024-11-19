d44c9dd0079673b3b06ce125d40e06817b9a0e93
sjs <wltkqdl@gmail.com>
2024-11-19 19:17:50 +0900

    fix: 코드에 ; 추가

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,5 +1,5 @@
-const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID
-const auth_code_path = process.env.REACT_APP_GITHUB_AUTH_CODE_PATH
+const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID;
+const auth_code_path = process.env.REACT_APP_GITHUB_AUTH_CODE_PATH;
 
 export const getGithubLoginLink = (): string => {
     const githubURL = `${auth_code_path}?client_id=${client_id}`;
