65840f6ac8d27de583498f80a6cfdcb89b5a09be
Yesssung <cjsrnr0504@naver.com>
2024-11-20 11:07:11 +0900

    로딩중 파일 애니메이션 추가

--- src/action/Loading.tsx
+++ src/action/Loading.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Lottie from "lottie-react";
+import loadingLottie from "../user_pages/Animation.json"; // 애니메이션 파일 경로
 
 const Loading: React.FC = () => {
-  return <div>로딩중..인데요 😢</div>;
+  const [showLoadingLottie, setShowLoadingLottie] = useState(true); // 기본값 true
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      {showLoadingLottie && (
+        <div className="flex flex-col items-center">
+          {/* 애니메이션 */}
+          <Lottie animationData={loadingLottie} className="w-64" />
+          {/* 로딩 멘트 */}
+          <p className="mt-4 text-lg text-gray-700">로딩중... 😢</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Loading;
