f9ce84dd06e3e7b37afd806276d46442699d988f
sjs <wltkqdl@gmail.com>
2024-11-25 09:51:40 +0900

    feat: 쿠키 처리 Home.tsx

--- src/user_pages/Home.tsx
+++ src/user_pages/Home.tsx
@@ -9,12 +9,21 @@ interface Genre {
   image: string;
 }
 
+interface UserInfo {
+  nickname: string;
+  status: string;
+  profile_url: string;
+}
+
 const Home: React.FC = () => {
   const navigate = useNavigate();
 
   // 쿠키 이름 배열을 전달하여 쿠키 값을 가져옵니다.
   const [cookies, setCookie, removeCookie] = useCookies(['id']);
   const [cookieValue, setCookieValue] = useState<string | null>(null);
+
+  // 회원 상태
+  const [userState, setUserState] = useState<string | null>(null);
 
   // 유저 유효성 검증
   const checkAuth = async (userId: number) => {
@@ -24,27 +33,53 @@ const Home: React.FC = () => {
     }
   };
 
+  // user status 정보 얻음(서버에 get 요청으로)
+  const getUserStatus = async (userId: number) => {
+    try {
+      // 백엔드 API 호출
+      const response = await fetch(`${process.env.REACT_APP_SPRING_URI}/api/users/${userId}/status`);
+      if (!response.ok) throw new Error("Failed to fetch profile data.");
+      let data : UserInfo = await response.json();
+      console.log("Fetched User Data: ", data);
+      console.log("Fetched User Data type: ", typeof data);
+
+      const userStatus = data.status;
+      setUserState(userStatus);
+
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
+
   useEffect(() => {
-    if (cookies.id === undefined || cookies.id === null) {
+    const cookieId = cookies.id;
+    if (cookieId === undefined || cookieId === null) {
+      // alert('sssssss');
       navigate('/');
     // 'id' 쿠키 값 가져오기
-    } else if (cookies.id) {
-      setCookieValue(cookies.id);
-    } else {
-      setCookieValue(null);
-    }
-
-    if (!checkAuth(cookies.id)) {
-      navigate('/');  // 유저 상태코드 유효하지 않으면 접근
-    }
-
-    if (cookies.id) {
-      setCookieValue(cookies.id);
+    } else if (cookieId) {
+      setCookieValue(cookieId);
+      getUserStatus(cookieId);
+      if (!checkAuth(cookieId)) {
+        navigate('/');  // 유저 상태코드 유효하지 않으면 접근
+      }
     } else {
       setCookieValue(null);
     }
 
   }, [cookies, navigate]); // cookies가 변경될 때마다 실행
+
+  // userState가 업데이트된 후에 실행되는 useEffect
+  useEffect(() => {
+    console.log('userState: ', userState);
+    if (userState === 'INACTIVE') {
+      navigate('/login');
+    }
+  }, [userState]); // userState 값이 변경될 때마다 실행
+
+
 
   console.log('cookieValue: ', cookieValue);
 
