bff6450f8e20632beac90a08085dd49352c6c3b0
sjs <wltkqdl@gmail.com>
2024-11-27 15:43:29 +0900

    fix: home 화면 redirect 주석

--- src/api/accessControl.ts
+++ src/api/accessControl.ts
@@ -3,6 +3,12 @@
 const AuthGuard = async (userId: number) => {
   console.log('userId: ', userId);
   console.log('server url: ', process.env.REACT_APP_SPRING_URI);
+
+  if (userId === undefined) {
+    console.error('유저 아이디 타입이 undefined 입니다.');
+    return false;
+  }
+
   try {
     const response = await fetch(`${process.env.REACT_APP_SPRING_URI}/api/users/${userId}`, {
       method: 'GET',
